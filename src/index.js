e03cf463be669fdd6b2334be397d1d756ec3b529
Hilary Claire <hilary.wilson2563@gmail.com>
2018-01-22 12:48:48 -0500

    developement

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import  Icon from './lakeandballoon.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
-
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  var btn = document.createElement('button');
  
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  
-  console.log(Data);
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
+  element.appendChild(btn);
+ 
   return element;
 }
 
